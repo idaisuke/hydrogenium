e0a59022a6cf217cb9e11ebb2261f541f8b7b89a
Daisuke Itabashi <itabashi.d@gmail.com>
2015-05-18 14:03:50 +0900

    Add build-libssl-for-android.sh

---
+++ build-tools/OpenSSL/Android.mk
@@ -0,0 +1,12 @@
+LOCAL_PATH := $(call my-dir)
+include $(CLEAR_VARS)
+
+LOCAL_EXPORT_C_INCLUDES := $(LOCAL_PATH)/$(TARGET_ARCH_ABI)/include
+
+LOCAL_MODULE := crypto
+LOCAL_SRC_FILES := $(TARGET_ARCH_ABI)/lib/libcrypto.a
+include $(PREBUILT_STATIC_LIBRARY)
+
+LOCAL_MODULE := ssl
+LOCAL_SRC_FILES := $(TARGET_ARCH_ABI)/lib/libssl.a
+include $(PREBUILT_STATIC_LIBRARY)
