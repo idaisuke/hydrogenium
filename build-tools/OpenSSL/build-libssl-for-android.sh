a277d52f248451d7a4ebe0c099c0c023a65570d5
Daisuke Itabashi <itabashi.d@gmail.com>
2016-02-16 16:12:51 +0900

    Clean scripts

--- build-tools/OpenSSL/build-libssl-for-android.sh
+++ build-tools/OpenSSL/build-libssl-for-android.sh
@@ -13,96 +13,97 @@ fi
 ################################################################################
 
 # OpenSSL Version
-OPENSSL_VERSION=1_0_2a
+OPENSSL_VERSION="1_0_2f"
 
 # Android Platform Version
-ANDROID_PLATFORM=android-19
+ANDROID_PLATFORM="android-21"
 
 ################################################################################
 #
 ################################################################################
 
 # OpenSSL Git Repository URL
-REPOSITORY_URL=git://github.com/openssl/openssl.git
+REPOSITORY_URL="git://github.com/openssl/openssl.git"
 
 # OpenSSL Git Repository Tag
-REPOSITORY_TAG=OpenSSL_$OPENSSL_VERSION
+REPOSITORY_TAG="OpenSSL_${OPENSSL_VERSION}"
 
 # Directory Path in which this script is located.
-SCRIPT_DIR=$(cd -P $(dirname $0) && pwd -P)
+SCRIPT_DIR="$(cd -P $(dirname $0) && pwd -P)"
 
 # OpenSSL Source Directory Path
-SRC_DIR=$SCRIPT_DIR/src/$OPENSSL_VERSION
+SRC_DIR="${SCRIPT_DIR}/src/${OPENSSL_VERSION}"
 
 # Output Directory Path
-OUTPUT_DIR=$SCRIPT_DIR/dest/$OPENSSL_VERSION/Android
+OUTPUT_DIR="${SCRIPT_DIR}/dest/${OPENSSL_VERSION}/Android"
 
 # Android NDK standalone-toolchain Path
-NDK_TOOLCHAIN_PATH=$OUTPUT_DIR/a1e1124d-8e65-444b-b19e-ba883168c4a8
+NDK_TOOLCHAIN_PATH="${OUTPUT_DIR}/a1e1124d-8e65-444b-b19e-ba883168c4a8"
 
 # Android NDK standalone-toolchain Path(ARM)
-ARM_TOOLCHAIN_PATH=$NDK_TOOLCHAIN_PATH/arm
+ARM_TOOLCHAIN_PATH="${NDK_TOOLCHAIN_PATH}/arm"
 
 # Android NDK standalone-toolchain Path(x86)
-X86_TOOLCHAIN_PATH=$NDK_TOOLCHAIN_PATH/x86
+X86_TOOLCHAIN_PATH="${NDK_TOOLCHAIN_PATH}/x86"
 
 # Android NDK standalone-toolchain Prefix(ARM)
-ARM_TOOLCHAIN_PREFIX=$ARM_TOOLCHAIN_PATH/bin/arm-linux-androideabi-
+ARM_TOOLCHAIN_PREFIX="${ARM_TOOLCHAIN_PATH}/bin/arm-linux-androideabi-"
 
 # Android NDK standalone-toolchain Prefix(x86)
-X86_TOOLCHAIN_PREFIX=$X86_TOOLCHAIN_PATH/bin/i686-linux-android-
+X86_TOOLCHAIN_PREFIX="${X86_TOOLCHAIN_PATH}/bin/i686-linux-android-"
 
 ################################################################################
 #
 ################################################################################
 
 # git clone
-rm -rf $SRC_DIR
-git clone -b $REPOSITORY_TAG $REPOSITORY_URL $SRC_DIR
+rm -rf "${SRC_DIR}"
+git clone -b "${REPOSITORY_TAG}" "${REPOSITORY_URL}" "${SRC_DIR}"
 
 ################################################################################
 #
 ################################################################################
 
 # make Android NDK standalone-toolchain(ARM)
-rm -rf $ARM_TOOLCHAIN_PATH
-$NDK_ROOT/build/tools/make-standalone-toolchain.sh --arch=arm --install-dir=$ARM_TOOLCHAIN_PATH --platform=$ANDROID_PLATFORM
+rm -rf "${ARM_TOOLCHAIN_PATH}"
+"${NDK_ROOT}/build/tools/make-standalone-toolchain.sh" --arch=arm --install-dir="${ARM_TOOLCHAIN_PATH}" --platform="${ANDROID_PLATFORM}"
 
 # make Android NDK standalone-toolchain(x86)
-rm -rf $X86_TOOLCHAIN_PATH
-$NDK_ROOT/build/tools/make-standalone-toolchain.sh --arch=x86 --install-dir=$X86_TOOLCHAIN_PATH --platform=$ANDROID_PLATFORM
+rm -rf "${X86_TOOLCHAIN_PATH}"
+"${NDK_ROOT}/build/tools/make-standalone-toolchain.sh" --arch=x86 --install-dir="${X86_TOOLCHAIN_PATH}" --platform="${ANDROID_PLATFORM}"
 
 ################################################################################
 #
 ################################################################################
 
-cd $SRC_DIR
+cd "${SRC_DIR}"
 
 # build for armeabi
-CONFIG=android
-PREFIX=$OUTPUT_DIR/armeabi
-CROSS_COMPILE_PREFIX=$ARM_TOOLCHAIN_PREFIX
-export ANDROID_DEV=$NDK_ROOT/platforms/$ANDROID_PLATFORM/arch-arm/usr
-./Configure --prefix=$PREFIX --cross-compile-prefix=$CROSS_COMPILE_PREFIX $CONFIG threads no-shared
+CONFIG="android"
+PREFIX="${OUTPUT_DIR}/armeabi"
+CROSS_COMPILE_PREFIX="${ARM_TOOLCHAIN_PREFIX}"
+echo "--prefix='${PREFIX}'"
+export ANDROID_DEV="${NDK_ROOT}/platforms/${ANDROID_PLATFORM}/arch-arm/usr"
+./Configure --prefix="'${PREFIX}'" --cross-compile-prefix="${CROSS_COMPILE_PREFIX}" "${CONFIG}" threads no-shared
 make -s && make install
 
 # build for armeabi-v7a
-CONFIG=android-armv7
-PREFIX=$OUTPUT_DIR/armeabi-v7a
-CROSS_COMPILE_PREFIX=$ARM_TOOLCHAIN_PREFIX
-export ANDROID_DEV=$NDK_ROOT/platforms/$ANDROID_PLATFORM/arch-arm/usr
+CONFIG="android-armv7"
+PREFIX="${OUTPUT_DIR}/armeabi-v7a"
+CROSS_COMPILE_PREFIX="${ARM_TOOLCHAIN_PREFIX}"
+export ANDROID_DEV="${NDK_ROOT}/platforms/${ANDROID_PLATFORM}/arch-arm/usr"
 make clean
-./Configure --prefix=$PREFIX --cross-compile-prefix=$CROSS_COMPILE_PREFIX $CONFIG threads no-shared
+./Configure --prefix="'${PREFIX}'" --cross-compile-prefix="${CROSS_COMPILE_PREFIX}" "${CONFIG}" threads no-shared
 make -s && make install
 
 # build for x86
-CONFIG=android-x86
-PREFIX=$OUTPUT_DIR/x86
-CROSS_COMPILE_PREFIX=$X86_TOOLCHAIN_PREFIX
-export ANDROID_DEV=$NDK_ROOT/platforms/$ANDROID_PLATFORM/arch-x86/usr
+CONFIG="android-x86"
+PREFIX="${OUTPUT_DIR}/x86"
+CROSS_COMPILE_PREFIX="${X86_TOOLCHAIN_PREFIX}"
+export ANDROID_DEV="${NDK_ROOT}/platforms/${ANDROID_PLATFORM}/arch-x86/usr"
 make clean
-./Configure --prefix=$PREFIX --cross-compile-prefix=$CROSS_COMPILE_PREFIX $CONFIG threads no-shared
+./Configure --prefix="'${PREFIX}'" --cross-compile-prefix="${CROSS_COMPILE_PREFIX}" "${CONFIG}" threads no-shared
 make -s && make install
 
-cp -f $SCRIPT_DIR/Android.mk $OUTPUT_DIR/Android.mk
-rm -rf $NDK_TOOLCHAIN_PATH
+cp -f "${SCRIPT_DIR}/Android.mk" "${OUTPUT_DIR}/Android.mk"
+rm -rf "${NDK_TOOLCHAIN_PATH}"
--- build-tools/SQLCipher/build-libsqlcipher-for-android.sh
+++ build-tools/SQLCipher/build-libsqlcipher-for-android.sh
@@ -13,10 +13,10 @@ fi
 ################################################################################
 
 # OpenSSL Version
-OPENSSL_VERSION=1_0_2a
+OPENSSL_VERSION="1_0_2f"
 
 # Android Platform Version
-ANDROID_PLATFORM=android-19
+ANDROID_PLATFORM="android-21"
 
 ################################################################################
 #
@@ -26,110 +26,110 @@ ANDROID_PLATFORM=android-19
 SCRIPT_DIR=$(cd -P $(dirname $0) && pwd -P)
 
 # OpenSSL Root Path
-OPENSSL_PATH=$SCRIPT_DIR/../OpenSSL/dest/$OPENSSL_VERSION/Android
+OPENSSL_PATH="${SCRIPT_DIR}/../OpenSSL/dest/${OPENSSL_VERSION}/Android"
 
 # OpenSSL Include Path(armeabi)
-OPENSSL_INCLUDE_PATH_ARM=$OPENSSL_PATH/armeabi/include
+OPENSSL_INCLUDE_PATH_ARM="${OPENSSL_PATH}/armeabi/include"
 
 # OpenSSL Library Path(armeabi)
-OPENSSL_LIBRARY_PATH_ARM=$OPENSSL_PATH/armeabi/lib
+OPENSSL_LIBRARY_PATH_ARM="${OPENSSL_PATH}/armeabi/lib"
 
 # OpenSSL Include Path(armeabi-v7a)
-OPENSSL_INCLUDE_PATH_ARMV7=$OPENSSL_PATH/armeabi-v7a/include
+OPENSSL_INCLUDE_PATH_ARMV7="${OPENSSL_PATH}/armeabi-v7a/include"
 
 # OpenSSL Library Path(armeabi-v7a)
-OPENSSL_LIBRARY_PATH_ARMV7=$OPENSSL_PATH/armeabi-v7a/lib
+OPENSSL_LIBRARY_PATH_ARMV7="${OPENSSL_PATH}/armeabi-v7a/lib"
 
 # OpenSSL Include Path(armeabi-v7a)
-OPENSSL_INCLUDE_PATH_X86=$OPENSSL_PATH/x86/include
+OPENSSL_INCLUDE_PATH_X86="${OPENSSL_PATH}/x86/include"
 
 # OpenSSL Library Path(armeabi-v7a)
-OPENSSL_LIBRARY_PATH_X86=$OPENSSL_PATH/x86/lib
+OPENSSL_LIBRARY_PATH_X86="${OPENSSL_PATH}/x86/lib"
 
 # SQLCipher Git Repository URL
-REPOSITORY_URL=git://github.com/sqlcipher/sqlcipher.git
+REPOSITORY_URL="git://github.com/sqlcipher/sqlcipher.git"
 
 # SQLCipher Source Directory Path
-SRC_DIR=$SCRIPT_DIR/src/sqlcipher
+SRC_DIR="${SCRIPT_DIR}/src/sqlcipher"
 
 # Output Directory Path
-OUTPUT_DIR=$SCRIPT_DIR/dest/Android
+OUTPUT_DIR="${SCRIPT_DIR}/dest/Android"
 
 # Android NDK standalone-toolchain Path
-NDK_TOOLCHAIN_PATH=$OUTPUT_DIR/d174a27c-c7aa-4fda-ad53-d1fea50d3117
+NDK_TOOLCHAIN_PATH="${OUTPUT_DIR}/d174a27c-c7aa-4fda-ad53-d1fea50d3117"
 
 # Android NDK standalone-toolchain Path(ARM)
-ARM_TOOLCHAIN_PATH=$NDK_TOOLCHAIN_PATH/arm
+ARM_TOOLCHAIN_PATH="${NDK_TOOLCHAIN_PATH}/arm"
 
 # Android NDK standalone-toolchain Path(x86)
-X86_TOOLCHAIN_PATH=$NDK_TOOLCHAIN_PATH/x86
+X86_TOOLCHAIN_PATH="${NDK_TOOLCHAIN_PATH}/x86"
 
 ################################################################################
 #
 ################################################################################
 
 # git clone
-rm -rf $SRC_DIR
-git clone $REPOSITORY_URL $SRC_DIR
+rm -rf "${SRC_DIR}"
+git clone "${REPOSITORY_URL}" "${SRC_DIR}"
 
 ################################################################################
 #
 ################################################################################
 
 # make Android NDK standalone-toolchain(ARM)
-rm -rf $ARM_TOOLCHAIN_PATH
-$NDK_ROOT/build/tools/make-standalone-toolchain.sh --arch=arm --install-dir=$ARM_TOOLCHAIN_PATH --platform=$ANDROID_PLATFORM
+rm -rf "${ARM_TOOLCHAIN_PATH}"
+"${NDK_ROOT}/build/tools/make-standalone-toolchain.sh" --arch=arm "--install-dir=${ARM_TOOLCHAIN_PATH}" "--platform=${ANDROID_PLATFORM}"
 
-find $ARM_TOOLCHAIN_PATH/bin -name arm-linux-androideabi* -type f | while read FILE
+find "${ARM_TOOLCHAIN_PATH}/bin" -name arm-linux-androideabi* -type f | while read FILE
 do
-  cp -f $FILE ${FILE%/*}/arm-linux${FILE##*-androideabi}
+  cp -f "${FILE}" "${FILE%/*}/arm-linux${FILE##*-androideabi}"
 done
 
 # make Android NDK standalone-toolchain(x86)
-rm -rf $X86_TOOLCHAIN_PATH
-$NDK_ROOT/build/tools/make-standalone-toolchain.sh --arch=x86 --install-dir=$X86_TOOLCHAIN_PATH --platform=$ANDROID_PLATFORM
+rm -rf "${X86_TOOLCHAIN_PATH}"
+"${NDK_ROOT}/build/tools/make-standalone-toolchain.sh" --arch=x86 "--install-dir=${X86_TOOLCHAIN_PATH}" "--platform=${ANDROID_PLATFORM}"
 
-find $X86_TOOLCHAIN_PATH/bin -name i686-linux-android* -type f | while read FILE
+find "${X86_TOOLCHAIN_PATH}/bin" -name i686-linux-android* -type f | while read FILE
 do
-  cp -f $FILE ${FILE%/*}/i686-linux${FILE##*-android}
+  cp -f "${FILE}" "${FILE%/*}/i686-linux${FILE##*-android}"
 done
 
 ################################################################################
 #
 ################################################################################
 
-cd $SRC_DIR
+cd "${SRC_DIR}"
 
 # build for armeabi
-ARCH=armv5
-PREFIX=$OUTPUT_DIR/armeabi
-HOST=arm-linux
-OPENSSL_INCLUDE_PATH=$OPENSSL_INCLUDE_PATH_ARM
-OPENSSL_LIBRARY_PATH=$OPENSSL_LIBRARY_PATH_ARM
-TOOLCHAIN_PATH=$ARM_TOOLCHAIN_PATH/bin
-PATH=$TOOLCHAIN_PATH:$PATH && ./configure --prefix=$PREFIX -host=$HOST --enable-tempstore=yes --disable-tcl --disable-readline CFLAGS="-DSQLITE_HAS_CODEC -DSQLITE_TEMP_STORE=2 -march=$ARCH -fPIE -I$OPENSSL_INCLUDE_PATH" LDFLAGS="-L$OPENSSL_LIBRARY_PATH"
+ARCH="armv5"
+PREFIX="${OUTPUT_DIR}/armeabi"
+HOST="arm-linux"
+OPENSSL_INCLUDE_PATH="${OPENSSL_INCLUDE_PATH_ARM}"
+OPENSSL_LIBRARY_PATH="${OPENSSL_LIBRARY_PATH_ARM}"
+TOOLCHAIN_PATH="${ARM_TOOLCHAIN_PATH}/bin"
+PATH="${TOOLCHAIN_PATH}:${PATH}" && ./configure --prefix="${PREFIX}" -host="${HOST}" --enable-tempstore=yes --disable-tcl --disable-readline CFLAGS="-DSQLITE_HAS_CODEC -DSQLITE_TEMP_STORE=2 -march=${ARCH} -fPIE -I${OPENSSL_INCLUDE_PATH}" LDFLAGS="-L${OPENSSL_LIBRARY_PATH}"
 make clean && make && make install
 
 # build for armeabi-v7a
-ARCH=armv7-a
-PREFIX=$OUTPUT_DIR/armeabi-v7a
-HOST=arm-linux
-OPENSSL_INCLUDE_PATH=$OPENSSL_INCLUDE_PATH_ARMV7
-OPENSSL_LIBRARY_PATH=$OPENSSL_LIBRARY_PATH_ARMV7
-TOOLCHAIN_PATH=$ARM_TOOLCHAIN_PATH/bin
-PATH=$TOOLCHAIN_PATH:$PATH && ./configure --prefix=$PREFIX -host=$HOST --enable-tempstore=yes --disable-tcl --disable-readline CFLAGS="-DSQLITE_HAS_CODEC -DSQLITE_TEMP_STORE=2 -march=$ARCH -fPIE -I$OPENSSL_INCLUDE_PATH" LDFLAGS="-L$OPENSSL_LIBRARY_PATH"
+ARCH="armv7-a"
+PREFIX="${OUTPUT_DIR}/armeabi-v7a"
+HOST="arm-linux"
+OPENSSL_INCLUDE_PATH="${OPENSSL_INCLUDE_PATH_ARMV7}"
+OPENSSL_LIBRARY_PATH="${OPENSSL_LIBRARY_PATH_ARMV7}"
+TOOLCHAIN_PATH="${ARM_TOOLCHAIN_PATH}/bin"
+PATH="${TOOLCHAIN_PATH}:${PATH}" && ./configure --prefix="${PREFIX}" -host="${HOST}" --enable-tempstore=yes --disable-tcl --disable-readline CFLAGS="-DSQLITE_HAS_CODEC -DSQLITE_TEMP_STORE=2 -march=${ARCH} -fPIE -I${OPENSSL_INCLUDE_PATH}" LDFLAGS="-L${OPENSSL_LIBRARY_PATH}"
 make clean && make && make install
 
 # build for x86
-ARCH=i686
-PREFIX=$OUTPUT_DIR/x86
-HOST=i686-linux
-OPENSSL_INCLUDE_PATH=$OPENSSL_INCLUDE_PATH_X86
-OPENSSL_LIBRARY_PATH=$OPENSSL_LIBRARY_PATH_X86
-TOOLCHAIN_PATH=$X86_TOOLCHAIN_PATH/bin
-PATH=$TOOLCHAIN_PATH:$PATH && ./configure --prefix=$PREFIX -host=$HOST --enable-tempstore=yes --disable-tcl --disable-readline CFLAGS="-DSQLITE_HAS_CODEC -DSQLITE_TEMP_STORE=2 -march=$ARCH -fPIE -I$OPENSSL_INCLUDE_PATH" LDFLAGS="-L$OPENSSL_LIBRARY_PATH"
+ARCH="i686"
+PREFIX="${OUTPUT_DIR}/x86"
+HOST="i686-linux"
+OPENSSL_INCLUDE_PATH="${OPENSSL_INCLUDE_PATH_X86}"
+OPENSSL_LIBRARY_PATH="${OPENSSL_LIBRARY_PATH_X86}"
+TOOLCHAIN_PATH="${X86_TOOLCHAIN_PATH}/bin"
+PATH="${TOOLCHAIN_PATH}:${PATH}" && ./configure --prefix="${PREFIX}" -host="${HOST}" --enable-tempstore=yes --disable-tcl --disable-readline CFLAGS="-DSQLITE_HAS_CODEC -DSQLITE_TEMP_STORE=2 -march=${ARCH} -fPIE -I${OPENSSL_INCLUDE_PATH}" LDFLAGS="-L${OPENSSL_LIBRARY_PATH}"
 make clean && make && make install
 
 
-cp -f $SCRIPT_DIR/Android.mk $OUTPUT_DIR/Android.mk
-rm -rf $NDK_TOOLCHAIN_PATH
+cp -f "${SCRIPT_DIR}/Android.mk" "${OUTPUT_DIR}/Android.mk"
+rm -rf "${NDK_TOOLCHAIN_PATH}"
